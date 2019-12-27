7829550ecb63b14036f286d42845f433a3e53ae2
Akilesh <akilesh.t.15@gmail.com>
2019-12-27 15:45:37 +0530

    Include checks for root and Magisk
    Proceed to MainActivity only if rooted and Magisk is detected.
    Update room version to 2.2.3

--- app/src/main/java/app/akilesh/qacc/SplashActivity.kt
+++ app/src/main/java/app/akilesh/qacc/SplashActivity.kt
@@ -2,7 +2,10 @@ package app.akilesh.qacc
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.topjohnwu.superuser.Shell
 
 
 class SplashActivity : AppCompatActivity() {
@@ -10,9 +13,44 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        //Try to get root access
+        Shell.su().exec()
 
+        if (!Shell.rootAccess()) {
+            Log.e("ACC-SU", "Unable to get root access")
+            /* Possible scenarios:
+             * The user might not have granted access
+             * User manually denied access in Magisk manager
+             * Device is not rooted
+             */
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Unable to get root access")
+                .setMessage("Please ensure that:\n1. Your device is rooted using Magisk.\n2. ${getString(R.string.app_name)} is granted root access.")
+                .setCancelable(false)
+                .setNegativeButton("Exit") { _, _ ->
+                    finish()
+                }
+                .create()
+                .show()
+        }
+        else {
+            //Detect Magisk
+            val result = Shell.su("[ -d /data/adb/magisk ]").exec()
+            if (result.isSuccess) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle("Magisk not detected!")
+                    .setCancelable(false)
+                    .setNegativeButton("Exit") { _, _ ->
+                        finish()
+                    }
+                    .create()
+                    .show()
+            }
+        }
     }
 }
